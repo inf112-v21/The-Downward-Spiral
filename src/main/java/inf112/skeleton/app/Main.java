84438b8496c00a5769688e9cb5c7bf173d8a251a
simen.eggebo <oBNoJxsTN99JsAkwoycH>
2021-02-28 17:39:38 +0100

    Added ability to discover host on LAN.

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -2,20 +2,21 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.esotericsoftware.kryonet.Client;
 import inf112.skeleton.app.network.RRServer;
 
 import java.util.Scanner;
 
 public class Main {
 
-    // Når koden kjøres venter den på input. Trykk enter for å starte server, eller skriv hva som helst for å starte client.
+    // Når koden kjøres venter den på input. Trykk enter for å finne server, skriv "s" for å starte server, "c" for å starte client.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        if (input.equals("")) {
+        if (input.equals("s")) {
             RRServer server = new RRServer();
-        } else {
+        } else if (input.equals("c")){
 
             RobotRally game = new RobotRally();
             Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
@@ -23,6 +24,9 @@ public class Main {
             cfg.setWindowedMode(600, 800);
 
             new Lwjgl3Application(game, cfg);
+        } else {
+            Client client = new Client();
+            System.out.println("Found server with IP: " + client.discoverHost(27960, 5000));
         }
     }
 }
