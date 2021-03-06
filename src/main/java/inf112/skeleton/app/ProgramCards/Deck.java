3f5798c70598fc1fee5779670324e27520c00bbe
IsaFoster <ifo002@uib.no>
2021-03-06 14:18:51 +0100

    removed textures from deck creation

--- src/main/java/inf112/skeleton/app/ProgramCards/Deck.java
+++ src/main/java/inf112/skeleton/app/ProgramCards/Deck.java
@@ -21,14 +21,14 @@ public class Deck {
     public void createDeck () {
         currentDeck = new ArrayList<>();
         // todo: give cards different priority
-        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "move_1", 1, new Texture(Gdx.files.internal("Cards/move_1.png"))));
-        for (int i = 0; i < 12; i++) currentDeck.add(new Card(0, "move_2", 2, new Texture(Gdx.files.internal("Cards/move_2.png"))));
-        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "move_3",3, new Texture(Gdx.files.internal("Cards/move_3.png"))));
-        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "move_-1",-1, new Texture(Gdx.files.internal("Cards/move_-1.png"))));
+        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "move_1", 1));
+        for (int i = 0; i < 12; i++) currentDeck.add(new Card(0, "move_2", 2));
+        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "move_3",3));
+        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "move_-1",-1));
         // rotation cards, movement = 0
-        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "left_turn", 0, new Texture(Gdx.files.internal("Cards/left_turn.png"))));
-        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "right_turn", 0, new Texture(Gdx.files.internal("Cards/right_turn.png"))));
-        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "u_turn",0, new Texture(Gdx.files.internal("Cards/u_turn.png"))));
+        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "left_turn", 0));
+        for (int i = 0; i < 18; i++) currentDeck.add(new Card(0, "right_turn", 0));
+        for (int i = 0; i < 6; i++) currentDeck.add(new Card(0, "u_turn",0));
         Collections.shuffle(currentDeck);
         }
 
