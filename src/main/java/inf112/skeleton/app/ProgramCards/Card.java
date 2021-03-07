b3bcc8eec977b03a084cb5c6186e6b794e2fb6a0
sondr <sondre_elias@hotmail.com>
2021-03-07 19:04:58 +0100

    moved klassediagramOblig2.drawio into Deliverables

--- src/main/java/inf112/skeleton/app/ProgramCards/Card.java
+++ src/main/java/inf112/skeleton/app/ProgramCards/Card.java
@@ -9,34 +9,30 @@ public class Card extends Actor {
     private final int priority;
     private final String type;
     private final int moves;
-    private final Texture texture;
 
     ArrayList<Card> cards;
 
-    public Card(int priority, String type, int moves, Texture texture) {
+    public Card(int priority, String type, int moves) {
         this.priority = priority;
         this.type = type;
         this.moves = moves;
-        this.texture = texture;
+
     }
 
-    public void setTexture(Texture texture) {
-    }
+
     public String toString() {
         return type;
     }
 
 
     public void add(int priority, String type) {
-        cards.add(new Card(priority, type, moves, texture));
+        cards.add(new Card(priority, type, moves));
     }
 
     public int getMoves() {
         return moves;
     }
 
-    public Texture getTexture() {
-        return texture;
-    }
+
 
 }
