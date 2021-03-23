bd28ef6d87227026163a2c020d7f5cad80d0be0d
sondr <sondre_elias@hotmail.com>
2021-03-23 19:42:51 +0100

    implemented buttons with hover

--- src/main/java/inf112/skeleton/app/screens/MenuScreen.java
+++ src/main/java/inf112/skeleton/app/screens/MenuScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.esotericsoftware.kryonet.Client;
 import inf112.skeleton.app.network.RRServer;
 
@@ -12,8 +13,28 @@ public class MenuScreen extends ScreenAdapter {
 
     RoboRallyGame game;
 
+    Texture activePlayButton;
+    Texture inactivePlayButton;
+    Texture activeConnectButton;
+    Texture inactiveConnectButton;
+    Texture activeHostButton;
+    Texture inactiveHostButton;
+
+    private static final int BUTTON_WIDTH = 300;
+    private static final int BUTTON_HEIGHT = 150;
+    private static final int PLAY_Y = 350;
+    private static final int CONNECT_Y = 200;
+    private static final int HOST_Y = 50;
+
+
     public MenuScreen(RoboRallyGame game) {
         this.game = game;
+        activePlayButton = new Texture("Menu/buttonPlayActive.png");
+        inactivePlayButton = new Texture("Menu/buttonPlayInactive.png");
+        activeConnectButton = new Texture("Menu/buttonConnectActive.png");
+        inactiveConnectButton = new Texture("Menu/buttonConnectInactive.png");
+        activeHostButton = new Texture("Menu/buttonHostActive.png");
+        inactiveHostButton = new Texture("Menu/buttonHostInactive.png");
     }
 
     @Override
@@ -67,11 +88,24 @@ public class MenuScreen extends ScreenAdapter {
         });
         }
 
+    public void buttonHover(Texture activeButton, Texture inactiveButton, int y){
+        int x = 600/2 - BUTTON_WIDTH / 2;
+        if (Gdx.input.getX() < x + BUTTON_WIDTH && Gdx.input.getX() > x && 800 - Gdx.input.getY() < y + BUTTON_HEIGHT && 800 - Gdx.input.getY() > y) {
+            game.batch.draw(activeButton, x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }else{
+            game.batch.draw(inactiveButton, x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
+    }
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, .25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+        buttonHover(activePlayButton, inactivePlayButton, PLAY_Y);
+        buttonHover(activeConnectButton, inactiveConnectButton, CONNECT_Y);
+        buttonHover(activeHostButton, inactiveHostButton, HOST_Y);
+
         game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .90f);
         game.font.draw(game.batch, "Click to host and play", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .65f);
         game.font.draw(game.batch, "Click to connect to a server", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .40f);
