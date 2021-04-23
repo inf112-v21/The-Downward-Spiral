1f9121d4cc916c77f9929c9833fb01d7bafec256
simen <simen@eggeboe.no>
2021-04-23 12:41:55 +0200

    Fixed error when client timed out while waiting to execute moves. Fixed by sending an empty packet back whenever it executes a move.

--- src/main/java/inf112/skeleton/app/NetworkConnection.java
+++ src/main/java/inf112/skeleton/app/NetworkConnection.java
@@ -7,13 +7,7 @@ import com.esotericsoftware.kryonet.Listener;
 import inf112.skeleton.app.ProgramCards.Card;
 import inf112.skeleton.app.network.ClassRegister;
 import inf112.skeleton.app.network.NetworkPlayer;
-import inf112.skeleton.app.network.packets.PacketAddPlayer;
-import inf112.skeleton.app.network.packets.PacketNewConnectionResponse;
-import inf112.skeleton.app.network.packets.PacketRemovePlayer;
-import inf112.skeleton.app.network.packets.PacketUpdatePosition;
-import inf112.skeleton.app.network.packets.PacketRespondHand;
-import inf112.skeleton.app.network.packets.PacketRequestHand;
-import inf112.skeleton.app.network.packets.PacketExecuteCard;
+import inf112.skeleton.app.network.packets.*;
 import inf112.skeleton.app.screens.GameScreen;
 
 import java.io.IOException;
@@ -89,15 +83,20 @@ public class NetworkConnection {
                     if (packet.playerID == client.getID()) {
                         GameScreen.localPlayer.executeCard(packet.card);
                         GameScreen.localPlayer.chosenCards.remove(0);
+                        if (GameScreen.localPlayer.chosenCards.size() < 1){
+                            GameScreen.hud.setCardsIsSent(false);
+                            GameScreen.localPlayer.selectableCards.clear();
+                            GameScreen.localPlayer.chosenCards.clear();
+                        }
                     } else {
                         networkPlayers.get(packet.playerID).executeCard(packet.card);
                     }
 
 
-
+                    // Client needs to send a packet back to prevent a timeout
                     client.setKeepAliveTCP(10*1000);
+                    client.sendTCP(new PacketKeepAlive());
 
-                    System.out.println(client.isIdle());
                     try {
                         TimeUnit.SECONDS.sleep(1);
                     } catch (InterruptedException e) {
@@ -105,11 +104,9 @@ public class NetworkConnection {
                     }
 
 
-                    if (GameScreen.localPlayer.chosenCards.size() < 1){
-                        GameScreen.hud.setCardsIsSent(false);
-                        GameScreen.localPlayer.selectableCards.clear();
-                        GameScreen.localPlayer.chosenCards.clear();
-                    }
+
+
+
 
                 }
             }
--- src/main/java/inf112/skeleton/app/network/ClassRegister.java
+++ src/main/java/inf112/skeleton/app/network/ClassRegister.java
@@ -2,13 +2,7 @@ package inf112.skeleton.app.network;
 
 import inf112.skeleton.app.Direction;
 import inf112.skeleton.app.ProgramCards.Card;
-import inf112.skeleton.app.network.packets.PacketAddPlayer;
-import inf112.skeleton.app.network.packets.PacketNewConnectionResponse;
-import inf112.skeleton.app.network.packets.PacketRemovePlayer;
-import inf112.skeleton.app.network.packets.PacketUpdatePosition;
-import inf112.skeleton.app.network.packets.PacketRespondHand;
-import inf112.skeleton.app.network.packets.PacketRequestHand;
-import inf112.skeleton.app.network.packets.PacketExecuteCard;
+import inf112.skeleton.app.network.packets.*;
 
 import java.util.ArrayList;
 
@@ -25,6 +19,7 @@ public class ClassRegister {
             com.badlogic.gdx.utils.Array.class,
             PacketRequestHand.class,
             PacketRespondHand.class,
-            PacketExecuteCard.class
+            PacketExecuteCard.class,
+            PacketKeepAlive.class
     };
 }
